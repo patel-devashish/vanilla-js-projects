61dedb8bf57870b2c5edfdbddcfbb518de5c641c
Devashish Patel <devashishpatel01@gmail.com>
2023-01-02 22:09:56 +0530

    Added functions in JS

--- calculator/script.js
+++ calculator/script.js
@@ -1,6 +1,32 @@
-const numberButtons = document.querySelectorAll('[data-number]')
-const operatioButtons = document.querySelectorAll('[data-operation]')
-const equalButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const clearButton = document.querySelector('[data-clear]')
-const screen = document.querySelector('[data-screen]')
+let numberButtons = document.querySelectorAll('[data-number]')
+let operatioButtons = document.querySelectorAll('[data-operation]')
+let equalButton = document.querySelector('[data-equals]')
+let deleteButton = document.querySelector('[data-delete]')
+let clearButton = document.querySelector('[data-clear]')
+let screen = document.querySelector('[data-screen]')
+
+function clear() {
+    screen.innerText = "\n";
+}
+
+function del() {
+
+}
+
+function appendNumber() {
+
+}
+
+function chooseoperation() {
+
+}
+
+function compute() {
+    
+}
+
+function updateDisplay() {
+
+}
+
+clearButton.addEventListener("click", clear);

52ba61d1fd6d3f8a519dec8b6ec676f69f1049a0
Devashish Patel <devashishpatel01@gmail.com>
2023-01-04 22:12:56 +0530

    Fixed appendNumber function

--- calculator/script.js
+++ calculator/script.js
@@ -1,40 +1,36 @@
-let numberButtons = document.querySelectorAll('[data-number]')
-let operatioButtons = document.querySelectorAll('[data-operation]')
-let equalButton = document.querySelector('[data-equals]')
-let deleteButton = document.querySelector('[data-delete]')
-let clearButton = document.querySelector('[data-clear]')
-let screen = document.querySelector('[data-screen]')
+let numberButtons = document.querySelectorAll("[data-number]");
+let operatioButtons = document.querySelectorAll("[data-operation]");
+let equalButton = document.querySelector("[data-equals]");
+let deleteButton = document.querySelector("[data-delete]");
+let clearButton = document.querySelector("[data-clear]");
+let screen = document.querySelector("[data-screen]");
 
 function clear() {
-    screen.innerText = "\n";
+  screen.innerText = "\n";
 }
 
 function del() {
-    screen.innerText = screen.innerText.slice(0, screen.innerText.length -1);
+  screen.innerText = screen.innerText.slice(0, screen.innerText.length - 1);
 }
 
 function appendNumber(btn) {
-    if (btn.innerText === '.' && screen.innerText.includes('.')) {
-        return
-    } 
-    else {
-        screen.innerText = screen.ineerText + btn.innerText;
+  btn.onclick = function () {
+    if (btn.innerText === "." && screen.innerText.includes(".")) {
+      return;
+    } else {
+      screen.innerText = screen.innerText + btn.innerText;
     }
-
+  };
 }
 
-function chooseoperation() {
+function chooseOperation() {}
 
-}
+function compute() {}
 
-function compute() {
-    
-}
-
-function updateDisplay() {
-
-}
+function updateDisplay() {}
 
 clearButton.addEventListener("click", clear);
 deleteButton.addEventListener("click", del);
-numberButtons.forEach((btn) => {btn.addEventListener("click", appendNumber(btn))});
+numberButtons.forEach((btn) => {
+  btn.addEventListener("click", appendNumber(btn));
+});

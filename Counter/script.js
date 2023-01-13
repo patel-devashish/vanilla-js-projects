3f905b0898a6e943686fbf5f4b3e26d44bbac81c
Devashish Patel <devashishpatel01@gmail.com>
2023-01-14 00:24:18 +0530

    Added colours to the digit(s) in the Counter project

--- Counter/script.js
+++ Counter/script.js
@@ -7,16 +7,35 @@ let val = 0;
 function decrease() {
   val -= 1;
   display.innerText = val;
+  if (val < 0) {
+    display.style.color = "red";
+  }
+  if (val > 0) {
+    display.style.color = "green";
+  }
+  if (val == 0) {
+    display.style.color = "rgb(1, 32, 40)";
+  }
 }
 
 function reset() {
   val = 0;
   display.innerText = val;
+  display.style.color = "rgb(1, 32, 40)";
 }
 
 function increase() {
   val += 1;
   display.innerText = val;
+  if (val > 0) {
+    display.style.color = "green";
+  }
+  if (val < 0) {
+    display.style.color = "red";
+  }
+  if (val == 0) {
+    display.style.color = "rgb(1, 32, 40)";
+  }
 }
 decbtn.addEventListener("click", decrease);
 resbtn.addEventListener("click", reset);
